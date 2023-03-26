ec02ff86726266137baa1ebfe51adced6f030c8d
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-03-26 18:18:20 +0300

    Assginment is done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function countDown(num){
+    while (num >=0){
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
+
+
+ 
+
+function writeCards(namesArray, eventName){
+   const messageArray = [];
+    for(let i=0; i<namesArray.length; i++){
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        messageArray.push(message);
+    }return messageArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
